08c624e0a6f088ca532e140f52b4d10e30cb55e1
Humberto Ramone <humbertorody@gmail.com>
2023-07-28 11:41:28 -0300

    refactor: Ajustando http status code do retorno do controller Usuarios

--- HSSolution.API/Controllers/UsuarioController.cs
+++ HSSolution.API/Controllers/UsuarioController.cs
@@ -19,11 +19,11 @@ public class UsuarioController : ControllerBase
     /// </summary>
     /// <returns>Dados do evento</returns>
     /// <response code="200"></response>
-    /// <response code="400"></response>
+    /// <response code="204"></response>
     /// <response code="500"></response>
     [HttpGet]
     [ProducesResponseType(200)]
-    [ProducesResponseType(400)]
+    [ProducesResponseType(204)]
     [ProducesResponseType(500)]
     public async Task<IActionResult> GetAllUsuarios()
     {
@@ -46,11 +46,11 @@ public class UsuarioController : ControllerBase
     /// <param name="idUsuario">Identificador do usuário</param>
     /// <returns>Dados do usuário</returns>
     /// <response code="200"></response>
-    /// <response code="404"></response>
+    /// <response code="204"></response>
     /// /// <response code="500"></response>
     [HttpGet("id")]
     [ProducesResponseType(200)]
-    [ProducesResponseType(404)]
+    [ProducesResponseType(204)]
     [ProducesResponseType(500)]
     public async Task<IActionResult> GetUsuarioById(int idUsuario)
     {
@@ -65,7 +65,7 @@ public class UsuarioController : ControllerBase
             }
             catch (Exception e)
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                return StatusCode(StatusCodes.Status500InternalServerError,
                   $"Erro ao tentar recuperar Usuário por Id.\nErro: {e.Message}");
             }
         }
