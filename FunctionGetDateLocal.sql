aa7d53246a20833348d3b8c754f12269a1862a54
Humberto Ramone <humberto.ramone@csprime.com.br>
2023-07-28 17:04:37 -0300

    fix: Removendo a pasta SQL da solucao

--- FunctionGetDateLocal.sql
+++
@@ -1,9 +0,0 @@
-Create or Alter Function [dbo].[GetDateLocal]()
-Returns DateTime
-With SchemaBinding
-As 
-Begin
-    Declare @DateTime DateTime = SysDateTimeOffSet() At Time Zone 'E. South America Standard Time'
-    Return @DateTime
-End
-Go
--- InsertUsuarioPerfil.sql
+++
@@ -1,7 +0,0 @@
-Insert Usuarios ( ID_Perfil, Nome, NR_CPF, Email, NR_Telefone, Login, Senha, FL_Habilitado, NR_UltimoAcesso, DT_UltimoAcesso, DT_Nascimento, DT_Cadastro, DT_Expiracao, NR_Tentativa, DT_UltimaTentativa )
-    Values (1, 'Humberto','48667837881','humberto@gmail.com','11998734098','humberto','123',1,1,'2023-07-28','1998-02-08','2023-07-28','2024-07-28',1, '2023-07-28')
-
-Go
-
-Insert Perfil
-    Values ('Administrador'),('Gerencial'),('TI - Tecnologia da Informação'),('Comercial')
