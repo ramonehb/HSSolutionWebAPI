25d0b545687adf1193ae319e63de9333f82f7537
Humberto Ramone <humberto.ramone@csprime.com.br>
2023-08-10 01:15:44 -0300

    feat: Incluindo mensagem quando nao localizar o usuario e mudando http response

--- HSSolution.API/Controllers/UsuarioController.cs
+++ HSSolution.API/Controllers/UsuarioController.cs
@@ -50,13 +50,13 @@ public class UsuarioController : ControllerBase
     /// <param name="idUsuario">Identificador do usuário</param>
     /// <returns>Dados do usuário</returns>
     /// <response code="200"></response>
-    /// <response code="204"></response>
-    /// /// <response code="400"></response>
-    /// /// <response code="500"></response>
+    /// <response code="400"></response>
+    /// <response code="404"></response>
+    /// <response code="500"></response>
     [HttpGet("id")]
     [ProducesResponseType(200)]
-    [ProducesResponseType(204)]
     [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
     [ProducesResponseType(500)]
     public async Task<IActionResult> GetUsuarioById(int idUsuario)
     {
@@ -65,7 +65,7 @@ public class UsuarioController : ControllerBase
             if (idUsuario == 0) return BadRequest("Informe o id do usuário para buscar.");
 
             var usuario = await _usuarioApplication.GetUsuarioByIdAsync(idUsuario);
-            if (usuario is null) return NoContent();
+            if (usuario is null) return NotFound("Usuário não localizado.");
 
             return Ok(usuario);
         }
