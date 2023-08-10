b3aff7a9b19df1bf085d698d3902699dd632a315
Humberto Ramone <humberto.ramone@csprime.com.br>
2023-08-10 17:41:14 -0300

    fix: alterando o name do classe de Autenticacao para Token

---
+++ HSSolution.Application/Dtos/TokenInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HSSolution.Application.Dtos;
+
+public class TokenInputModel
+{
+
+    [Required(ErrorMessage = "O campo username é obrigatório."),
+        StringLength(25, MinimumLength = 3, ErrorMessage = "O username deve ter no mínimo 3 ou no máximo 25 carecteres.")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "O campo password é obrigatório."),
+        StringLength(20, MinimumLength = 3, ErrorMessage = "O password deve ter no mínimo 3 ou no máximo 20 carecteres.")]
+    public string Password { get; set; }
+}
